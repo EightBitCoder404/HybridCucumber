06d040c588605a85aa40e3db1fabd816b0e8529f
chandreshkmwt <chandreshkmwt@gmail.com>
2021-09-30 15:53:05 +0530

    First Commit

---
+++ HybridSeleniumCucumber/src/test/java/com/testcases/PageTest.java
@@ -0,0 +1,31 @@
+package com.testcases;
+
+import java.io.IOException;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.baseclass.Library;
+import com.pages.LoginPage;
+import com.reusablefunctions.SeleniumUtilities;
+
+public class PageTest extends Library {
+	@BeforeTest
+    public void stratUp() {
+        launchApplication("chrome","https://opensource-demo.orangehrmlive.com/");
+       
+    }
+    @Test
+    public void login() {
+        LoginPage lpage=new LoginPage(driver);
+        lpage.login("Admin", "admin123");
+       
+    }
+    @AfterClass
+    public void close() throws IOException {
+        SeleniumUtilities sUti = new SeleniumUtilities(driver);
+        sUti.to_take_screenshot("C:\\Users\\Chandreshwar\\eclipse-workspace\\HybridSeleniumCucumber\\src\\main\\resources\\Screenshots\\orangeHRMLogin.png");
+        quit();
+    }
+}
